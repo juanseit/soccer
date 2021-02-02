064463571685a00d0b6f4da612dcf36d864a957b
Juan Iturvide <76930767+juanseit@users.noreply.github.com>
2021-02-02 11:22:54 -0300

    Update xg.R

--- xg.R
+++ xg.R
@@ -21,7 +21,7 @@ ggplot(mapping = aes(x = xG, y = GP)) +
   geom_abline(slope = 1.244, intercept = -10.713) +
   labs( x = "Expected Goals.",
         y = "Goals Marcados.",
-        title = "Relação entre gols marcados e esperados em 19/20.",
+        title = "RelaÃ§Ã£o entre gols marcados e esperados em 19/20.",
         caption = "Data by Statsbomb and fbref.com") +
   theme_bw() +
   theme(
@@ -40,7 +40,7 @@ dados %>%
   geom_abline(slope = 1.396, intercept = -18.781) +
   labs( x = "Expected Goals em 2018/2019.",
         y = "Goals Marcados em 2019/2020.",
-        title = "Relação entre os gols esperados em 18/19 e os marcados em 19/20.",
+        title = "RelaÃ§Ã£o entre os gols esperados em 18/19 e os marcados em 19/20.",
         caption = "Data by Statsbomb and fbref.com") +
   theme_bw() +
   theme(
@@ -59,7 +59,7 @@ dados %>%
   geom_abline(slope = 1.064, intercept = -3.916) +
   labs( x = "Expected Goals em 2018/2019.",
         y = "Expected Goals em 2019/2020.",
-        title = "Relação entre os gols esperados em 18/19 e os em 19/20.",
+        title = "RelaÃ§Ã£o entre os gols esperados em 18/19 e os em 19/20.",
         caption = "Data by Statsbomb and fbref.com") +
   theme_bw() +
   theme(
@@ -69,22 +69,6 @@ dados %>%
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
 #gmae
-
-gmae %>%
-  ggplot(mapping = aes(x = Sh, y = Pgmae)) +
-  geom_point(aes(size = Gols), alpha = .6) +
-  geom_text(aes(label = Nome, vjust = -1)) +
-  labs( x = "Finalizações.",
-        y = "Porcentagem de gols marcados acima do esperado.",
-        title = "Eficiência de finalizações dos jogadores do Brasileirão 2020.",
-        subtitle = "Mínimo 25 chutes para ser qualificado.",
-        caption = "Data by Infogol.com and Globoesporte.") +
-  theme_bw() +
-  theme(
-    plot.title = element_text(size = 14, hjust = 0.5, face = "bold")
-  ) +
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
 reg1 <- gmae %>%
   lm(Gols ~ Pgmae, data = .) %>%
@@ -98,7 +82,7 @@ gmae %>%
   labs( x = "Gols.",
         y = "Porcentagem de gols marcados acima do esperado.",
         title = "Teste de R quadrado.",
-        subtitle = "Mínimo 25 chutes para ser qualificado.",
+        subtitle = "MÃ­nimo 25 chutes para ser qualificado.",
         caption = "Data by Infogol.com and Globoesporte.") +
   theme_bw() +
   theme(
@@ -106,54 +90,6 @@ gmae %>%
   ) +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
-
-#o que importa mais, ataque ou defesa?
-
-totals <- read_xlsx("C:/Users/User/Downloads/prem2.xlsx")
-
-ataque <- lm(Ptt ~ xGt, data = totals)
-summary(ataque)
-defesa <- lm(Ptt ~ xGAt, data = totals)
-summary(defesa)
-
-totals %>% #ataque
-  ggplot(mapping = aes(x = xGt, y = Ptt)) +
-  geom_smooth() +
-  geom_point(alpha = .6) +
-  geom_abline(slope =  1.18898, intercept = -13.40044) +
-  geom_text(aes(label = Equipe, vjust = -1)) +
-  labs( x = "Gols esperados marcados no total.",
-        y = "Pontos totais conquistados.",
-        title = "Pontos e Gols esperados acumulados na Premier League (2017/18 a 2019/20).",
-        caption = "Gráfico: @juanseit_ | Data by fbref.com and Statsbomb.") +
-  theme_bw() +
-  theme(
-    plot.title = element_text(size = 14, hjust = 0.5, face = "bold")
-  ) +
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
-
-totals %>% #ataque
-  ggplot(mapping = aes(x = xGAt, y = Ptt)) +
-  geom_smooth() +
-  geom_point(alpha = .6) +
-  geom_abline(slope =  0.8229, intercept = 28.1172) +
-  geom_text(aes(label = Equipe, vjust = -1)) +
-  labs( x = "Gols esperados sofridos no total.",
-        y = "Pontos totais conquistados.",
-        title = "Pontos e Gols esperados sofridos acumulados na Premier League (2017/18 a 2019/20).",
-        caption = "Gráfico: @juanseit_ | Data by fbref.com and Statsbomb.") +
-  theme_bw() +
-  theme(
-    plot.title = element_text(size = 14, hjust = 0.5, face = "bold")
-  ) +
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
-
-defesa_sem_6 <- totals %>%
-  filter(!Equipe %in% c("Liverpool", "Manchester City", "Manchester Utd", "Tottenham", "Chelsea", "Arsenal")) %>%
-  lm(Ptt ~ xGAt, data = .) %>%
-  summary()
 
 
   
