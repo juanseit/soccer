ebef830590e90fe8bada556f480b42e817dd9d2f
Juan Iturvide <76930767+juanseit@users.noreply.github.com>
2021-02-02 01:11:26 -0300

    Update medias_athletico.R

--- medias_athletico.R
+++ medias_athletico.R
@@ -7,7 +7,7 @@ df_cap <- read_xlsx("C:/Users/User/Documents/cap.xlsx")
 
 
 df_cap %>%
-  mutate(mov_xg = slide_dbl(media_xg, mean, .before = 10, complete = T), mov_xga = slide_dbl(media_xga, mean, .before = 3, complete = T)) %>%
+  mutate(mov_xg = slide_dbl(media_xg, mean, .before = 10, complete = T), mov_xga = slide_dbl(media_xga, mean, .before = 10, complete = T)) %>%
   ggplot() +
   geom_vline(xintercept = 6, linetype = "dashed", color = "white") +
   geom_vline(xintercept = 17, linetype = "dashed", color = "white") +
@@ -20,11 +20,11 @@ df_cap %>%
   geom_text(x=2.5, y=1.4, label="Dorival Jr.", color = "white", family = "Century Gothic") +
   geom_text(x=11.5, y=0.75, label="Eduardo Barros", color = "white", family = "Century Gothic") +
   geom_text(x=22.5, y=0.75, label="Paulo Autuori", color = "white", family = "Century Gothic") +
-  labs(title = "MÈdias de <span style = 'color:#00CCFF;'>xG</span> e <span style = 'color:red;'>xGA</span> do Athletico.",
-       subtitle = "Brasileir„o 2020/21. MÈdias mÛveis de 10 jogos.",
+  labs(title = "M√©dias de <span style = 'color:#00CCFF;'>xG</span> e <span style = 'color:red;'>xGA</span> do Athletico.",
+       subtitle = "Brasileir√£o 2020/21. M√©dias m√≥veis de 10 jogos.",
        x = "Rodadas.",
-       y = "MÈdias de xG e xGA por jogo.",
-       caption = "Gr·fico: @juanseit_ | Data by Infogol and Fivethirtyeight") +
+       y = "M√©dias de xG e xGA por jogo.",
+       caption = "Gr√°fico: @juanseit_ | Data by Infogol and Fivethirtyeight") +
   theme_minimal() +
   theme(legend.position = "right", 
         panel.grid.minor = element_blank(),
