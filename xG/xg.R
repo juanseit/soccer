b2db23dddf81ce89ee5bc85d3cd4a07294072e1f
Juan Iturvide <76930767+juanseit@users.noreply.github.com>
2021-02-10 17:17:01 -0300

    Update xg.R

--- xG/xg.R
+++ xG/xg.R
@@ -15,15 +15,15 @@ summary(fit_3)
 
 dados %>%
 ggplot(mapping = aes(x = xG, y = GP)) +
-  geom_point(color = "#91c390", size =  4) +
-  geom_text_repel(aes(label= Equipe), colour = "black") +
-  stat_smooth(method = "lm", color = "#359fda") +
+  geom_point(fill = "#8b0000", color = "white", size =  4, pch = 21) +
+  geom_text_repel(aes(label= Equipe), colour = "white") +
+  stat_smooth(method = "lm", color = "white", se = FALSE) +
   labs( x = "Expected Goals.",
         y = "Goals Marcados.",
         title = "Relação entre gols marcados e esperados em 19/20.",
         subtitle = "Utilizando times da Premier League.",
         caption = "Gráfico: @juanseit_ | Data by Statsbomb and fbref.com") +
-  theme_539() +
+  theme_theathletic() +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
@@ -33,15 +33,15 @@ ggsave("xg_1.png", width = 11, height = 8)
 
 dados %>%
   ggplot(mapping = aes(x = xGt, y = GP)) +
-  geom_point(color = "#91c390", size =  4) +
-  geom_text_repel(aes(label= Equipe), colour = "black") +
-  stat_smooth(method = "lm", color = "#359fda") +
+  geom_point(fill = "#8b0000", color = "white", size =  4, pch = 21) +
+  geom_text_repel(aes(label= Equipe), colour = "white") +
+  stat_smooth(method = "lm", color = "white", se = FALSE) +
   labs( x = "Expected Goals em 2018/2019.",
         y = "Goals Marcados em 2019/2020.",
         title = "Relação entre os gols esperados em 18/19 e os marcados em 19/20.",
         subtitle = "Utilizando times da Premier League.",
         caption = "Gráfico: @juanseit_ | Data by Statsbomb and fbref.com") +
-  theme_539()+
+  theme_theathletic()+
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
@@ -51,15 +51,15 @@ ggsave("xg_2.png", width = 11, height = 8)
 
 dados %>%
   ggplot(mapping = aes(x = xGt, y = xG)) +
-  geom_point(color = "#91c390", size =  4) +
-  geom_text_repel(aes(label= Equipe), colour = "black") +
-  stat_smooth(method = "lm", color = "#359fda") + 
+   geom_point(fill = "#8b0000", color = "white", size =  4, pch = 21) +
+  geom_text_repel(aes(label= Equipe), colour = "white") +
+  stat_smooth(method = "lm", color = "white", se = FALSE) +
   labs( x = "Expected Goals em 2018/2019.",
         y = "Expected Goals em 2019/2020.",
         title = "Relação entre os gols esperados em 18/19 e os em 19/20.",
         subtitle = "Utilizando times da Premier League.",
         caption = "Gráfico: @juanseit_ | Data by Statsbomb and fbref.com") +
-  theme_539()+
+  theme_theathletic()+
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
@@ -73,14 +73,14 @@ reg1 <- gmae %>%
 
 gmae %>%
   ggplot(mapping = aes(x = Sh, y = Pgmae)) +
-  geom_point(aes(size = Gols), color =  "#91c390", alpha = .6) +
-  geom_text_repel(aes(label = Nome), colour = "black") +
+  geom_point(aes(size = Gols), fill = "#8b0000", color = "white", size =  4, pch = 21) +
+  geom_text_repel(aes(label = Nome), colour = "white") +
   labs( x = "Finalizações.",
         y = "Porcentagem de gols marcados acima do esperado.",
         title = "Eficiência de finalizações dos jogadores do Brasileirão 2020.",
         subtitle = "Mínimo 25 chutes para ser qualificado.",
-        caption = "Data by Infogol.com and Globoesporte.") +
-  theme_539() +
+        caption = "Gráfico: @juanseit_ | Data by Infogol.com and Globoesporte.") +
+  theme_theathletic() +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
@@ -92,15 +92,15 @@ reg1 <- gmae %>%
 
 gmae %>%
   ggplot(mapping = aes(x = Pgmae, y = Gols)) +
-  geom_point(alpha = .6, size = 4, color = "#91c390") +
-  geom_abline(slope = c(.5,0.75,1,1.25,1.5), intercept = 0, linetype = "dashed", color = "#359fda") +
-  geom_text_repel(aes(label = Nome)) +
+  geom_point(geom_point(aes(size = Gols), fill = "#8b0000", color = "white", size =  4, pch = 21) +
+  geom_text_repel(aes(label = Nome), colour = "white") + 
+  geom_abline(slope = c(.5,0.75,1,1.25,1.5), intercept = 0, linetype = "dashed", color = "white") +
   labs( x = "Gols.",
         y = "Porcentagem de gols marcados acima do esperado.",
         title = "Em quanto os melhores finalizadores sobreperformaram os gols marcados.",
         subtitle = "Mínimo 25 chutes para ser qualificado.",
         caption = "Data by Infogol.com and Globoesporte.") +
-  theme_539(
+  theme_theathletic(
   ) +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
