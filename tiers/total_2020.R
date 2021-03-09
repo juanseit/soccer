a7eb7d4bb8d012f3e15ceb2f14c42843497233c2
Juan Iturvide <76930767+juanseit@users.noreply.github.com>
2021-03-09 01:11:48 -0300

    Update total_2020.R

--- tiers/total_2020.R
+++ tiers/total_2020.R
@@ -18,16 +18,20 @@ visitante <- df_538 %>%
   filter(season == 2020, league == "Brasileiro Série A", !is.na(xg2), !is.na(xg1)) %>%
   group_by(team2) %>%
   summarize(xg_fora = sum(xg2, na.rm = T), xga_fora = sum(xg1, na.rm = T)) %>%
-  ungroup()
+  ungroup() %>%
+  left_join(team_logo_brasileiro, by = c("team2"))
 
-mandante %>%
+geral_xg <- mandante %>%
   inner_join(visitante, by = c("team1" = "team2")) %>%
   group_by(team1) %>%
   summarize(total_xg = (xg_casa + xg_fora)/38, total_xga = (xga_casa + xga_fora)/38) %>%
-  ungroup() %>%
+  ungroup()
+  
+geral_xg %>%
   ggplot(mapping = aes(x = total_xg, y = total_xga)) +
+  geom_hline(yintercept = mean(geral_xg$total_xg), color = "white", linetype = "dashed", size = 1) +
+  geom_vline(xintercept = mean(geral_xg$total_xga), color = "white", linetype = "dashed", size = 1) +
   geom_image(aes(image = mandante$team_logo_wikipedia), asp = 16 / 11) +
-  geom_abline(slope = -1.5, intercept = c(0,.5,1, 1.5,2,2.5,3), alpha = .4, color = "white") +
   labs(x = "xG/Partida.",
        y = "xGA/Partida.",
        title = "Desempenho esperado do Brasileirão 2020/21.",
@@ -37,7 +41,7 @@ mandante %>%
   scale_y_reverse(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
-ggsave("total_xg.png", width = 11, height = 9, dpi = 300)
+ggsave("total_xg_logo.png", width = 12, height = 9, dpi = 300)
 
 mandante_nsxg <- df_538 %>%
   filter(season == 2020, league == "Brasileiro Série A", !is.na(xg2), !is.na(xg1)) %>%
@@ -50,16 +54,21 @@ visitante_nsxg <- df_538 %>%
   filter(season == 2020, league == "Brasileiro Série A", !is.na(xg2), !is.na(xg1)) %>%
   group_by(team2) %>%
   summarize(xg_fora = sum(nsxg2, na.rm = T), xga_fora = sum(nsxg1, na.rm = T)) %>%
-  ungroup()
+  ungroup() %>%
+  left_join(team_logo_brasileiro, by = c("team2"))
 
-mandante_nsxg %>%
+
+geral_nsxg <- mandante_nsxg %>%
   inner_join(visitante_nsxg, by = c("team1" = "team2")) %>%
   group_by(team1) %>%
   summarize(total_nsxg = (xg_casa + xg_fora)/38, total_nsxga = (xga_casa + xga_fora)/38) %>%
-  ungroup() %>%
+  ungroup()
+
+geral_nsxg %>%
   ggplot(mapping = aes(x = total_nsxg, y = total_nsxga)) +
+  geom_hline(yintercept = mean(geral_nsxg$total_nsxg), color = "white", linetype = "dashed", size = 1) +
+  geom_vline(xintercept = mean(geral_nsxg$total_nsxga), color = "white", linetype = "dashed", size = 1) +
   geom_image(aes(image = mandante_nsxg$team_logo_wikipedia), asp = 16 / 11) +
-  geom_abline(slope = -1.5, intercept = c(0.25,0.75,1.25,1.75), alpha = .4, color = "white") +
   labs(x = "NSxG/Partida.",
        y = "NSxGA/Partida.",
        title = "Desempenho esperado do Brasileirão 2020/21.",
@@ -69,4 +78,4 @@ mandante_nsxg %>%
   scale_y_reverse(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
-ggsave("total_nsxg.png", width = 11, height = 9, dpi = 300)
+ggsave("total_nsxg_logo.png", width = 12, height = 9, dpi = 300)
